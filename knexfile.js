19861a66dbf1af5eae979047d33f4d04eb7e52a1
Adam Mescher <admescher@gmail.com>
2017-12-01 10:29:31 -0700

    Update knex

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,22 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'process.env.DATABASE_URL + `?ssl=true`',
+    connection: 'postgres://localhost/',
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/dev'
-    }
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL + `?ssl=true`',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   },
   useNullAsDefault: true
 };
